d67bb445e6bc5c6dc811684eb739bbd094f1ecba
CML120 <cheelor922@gmail.com>
2023-07-30 12:07:32 -0500

    added code to App.js and Header.js for navigation

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,23 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <AboutMe />
+      <Portfolio />
+      <Contact />
+      <Resume />
+      <Footer />
     </div>
   );
 }
